0d465c2ffd98841e14aaa72417e66823ebf01078
José Antonio Barranquero <joseantbarranquero@gmail.com>
2018-10-03 00:37:39 +0200

    First commit, functional skeleton, needs cleanup

---
+++ CoolBoy/frmMain.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace CoolBoy
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbLog = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbLog
+            // 
+            this.lbLog.FormattingEnabled = true;
+            this.lbLog.Location = new System.Drawing.Point(12, 12);
+            this.lbLog.Name = "lbLog";
+            this.lbLog.Size = new System.Drawing.Size(401, 290);
+            this.lbLog.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(65, 316);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(282, 316);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 351);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lbLog);
+            this.Name = "frmMain";
+            this.Text = "CoolBoy Server";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        public System.Windows.Forms.ListBox lbLog;
+    }
+}
+
---
+++ CoolBoy/frmMain.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace CoolBoy
+{
+    public partial class frmMain : Form
+    {
+        private WebServer server;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            server = new WebServer(SendResponse, "http://*/");
+
+            LogInfo("Attempting to start server on *");
+            try
+            {
+                server.Run();
+                btnStart.Enabled = false;
+                btnStop.Enabled = true;
+                LogInfo("Server started!");
+            }
+            catch (Exception ex)
+            {
+                LogInfo(string.Format("Server start failed: {0}", ex.Message));
+            }
+        }
+
+        public string SendResponse(HttpListenerRequest request)
+        {
+            LogInfo(string.Format("Serving {0}", request.RemoteEndPoint.Address));
+            return string.Format("<HTML><BODY>My web page.<br>{0}</BODY></HTML>", DateTime.Now);
+        }
+
+        public string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        public void LogInfo(string message)
+        {
+            if (lbLog.InvokeRequired)
+            {
+                lbLog.Invoke(new MethodInvoker(delegate
+                {
+                    lbLog.Items.Add(string.Format("[{0}] - {1}", DateTime.Now, message));
+                }));
+            }
+            else
+            {
+                lbLog.Items.Add(string.Format("[{0}] - {1}", DateTime.Now, message));
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            server.Stop();
+
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+
+            LogInfo("Server stopped");
+        }
+    }
+}
